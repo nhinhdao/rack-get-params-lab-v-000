4d82d5a80aa9f801971140c35ad5d51b2004063f
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-05 15:48:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,30 +1,42 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
+    @@items = ["Apples","Carrots","Pears"]
+    @@cart = ["Apples", "Pears"]
 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
+        if req.path.match(/items/)
+            @@items.each do |item|
+                resp.write "#{item}\n"
+            end
+        elsif req.path.match(/search/)
+            search_term = req.params["q"]
+            resp.write handle_search(search_term)
+        elsif req.path.match(/cart/)
+            @@cart.each do |item|
+                resp.write "#{item}\n"
+            end
+        elsif req.path.match(/add/)
+            search_term = req.params["item"]
+            if @@items.include?(search_term)
+                @@cart << search_term
+            else
+                return "Couldn't find #{search_term}"
+            end
+        else
+            resp.write "Path Not Found"
+        end
+
+        resp.finish
     end
 
-    resp.finish
-  end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
+    def handle_search(search_term)
+        if @@items.include?(search_term)
+            return "#{search_term} is one of our items"
+        else
+            return "Couldn't find #{search_term}"
+        end
     end
-  end
 end
