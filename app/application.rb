0da7d76bcc1bffd567e93e948effece746949c37
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-05 15:51:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,7 @@ class Application
             search_term = req.params["item"]
             if @@items.include?(search_term)
                 @@cart << search_term
+                return "added #{search_term}"
             else
                 return "We don't have that item"
             end
